4e1f9af681e4aa3f4e5e32afde69d606e7061d95
Shubham Nigave <143946315+shubhamnigave@users.noreply.github.com>
2024-07-29 18:54:04 +0530

    code added Day 1

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+class Hello {
+    public static void main(String a[])
+    {
+        int num1 = 5, num2 = 8;
+        
+        int result = num1 + num2;
+        System.out.print(result);
+    }
+}
