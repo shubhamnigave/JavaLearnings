68359ba43c18cd1c7054da91a984137749f6b903
Shubham Nigave <143946315+shubhamnigave@users.noreply.github.com>
2024-10-19 19:53:50 +0530

    Update ServicePlan.java
    comments added

--- CustomerManagementSystem/src/com/customer/core/ServicePlan.java
+++ CustomerManagementSystem/src/com/customer/core/ServicePlan.java
@@ -1,16 +1,14 @@
 package com.customer.core;
 
 public enum ServicePlan {
-	SILVER(1000),GOLD(2000),DIAMOND(5000),PLATINUM(10000);
+	SILVER(1000),GOLD(2000),DIAMOND(5000),PLATINUM(10000);//Created Custom Datatype ServicePlan and used is customer.java
 	private double amount;
 	ServicePlan(double amount){
 		this.amount = amount;
 	}
-	
 	public double getAmount() {
 		return amount;
 	}
-
 	@Override
 	public String toString() {
 		return name().toLowerCase()+ " amount: "+amount;
